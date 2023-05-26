8a936b20077e8f0f12076624da8188e81e8ea3be
Max Thilo <129679995+mlbt500@users.noreply.github.com>
2023-05-26 10:57:48 +0100

    health data arranged

--- world_bank_educatio&health_data.R
+++ world_bank_educatio&health_data.R
@@ -1,16 +1,11 @@
 list.files("data")
 ed_spending <- read.csv(".\\data\\education_spending.csv") ## World Bank
 health_spending1 <- read.csv(".\\data\\health_spending.csv") ## world Bank
+
 health_spending2 <- health_spending[,c(2, 45:64)]
 JBM_peers <- c("GBR", "AUT", "CAN", "DNK", "DEU", "FIN", "FRA", "NLD", "NOR", "SWE", "SVN", "USA")
 health_spending3 <- health_spending2[health_spending$Country.Code %in% JBM_peers, ]
-
-
-unique(ed_spending$LOCATION)
-
-head(health_spending)
-[health_spending$Country.Name == JBM_peers]
-ncol(health_spending)
-
-head(health_spending)
+means <- colMeans(health_spending3[,2:21])
+average_row <- c("AVE", means)
+health_spending4 <- rbind(health_spending3, average_row)
 
